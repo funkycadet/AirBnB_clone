41aaca0e52fb45297956821785f6436fba1d2f8a
smith2eric <ericsmithsonian2@gmail.com>
2022-08-08 10:54:46 +0100

    [fix]: Made changes to the methods in BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -37,9 +37,9 @@ This class defines all common attributes/methds for other classes:
               of our BaseModel
 
 """
-from datetime import date, datetime
-from uuid import uuid4
-# from models import storage
+from datetime import datetime
+import uuid
+from models import storage
 
 
 class BaseModel:
@@ -56,53 +56,54 @@ class BaseModel:
 
         """
         if not kwargs:
-          self.id = str(uuid4())
-          self.created_at = datetime.now
-          self.updated_at = self.created_at
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
         else:
-          f = "%Y-%m-%dT%H:%M:%S.%f"
-          for key, value in kwargs.items():
-            if key == 'created_at' or key == 'updated_at':
-              value = datetime.strptime(kwargs[key], f)
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
             if key != '__class__':
-              setattr(self, key, value)
+                setattr(self, key, value)
 
     def __str__(self):
-      """__str__ method
+        """__str__ method
 
-      Method to print object instance of BaseModel class
+        Method to print object instance of BaseModel class
 
-      """
-      class_name = "[" + self.__class__.name__ + "]"
-      dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-      return class_name + " (" + self.id + ") " + str(dct)
+        """
+        class_name = "[" + self.__class__.name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
 
     def save(self):
-      """save method
+        """save method
 
-      Method to update last updated time
+        Method to update last updated time
 
-      """
-      self.updated_at = datetime.now
-      # storage.save()
+        """
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-      """to_dict method
+        """to_dict method
 
-      Method to return a dictionary with all keys/values of __dict__ of the
-      instance
+        Method to return a dictionary with all keys/values of __dict__ of the
+        instance
 
-      """
-      new_dict = {}
+        """
+        new_dict = {}
 
-      for key, values in self.__dict__.items():
-        if key == "created_at" or key == "updated_at":
-          new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        else:
-          if not values:
-            pass
-          else:
-            new_dict[key] = values
-      new_dict['__class__'] = self.__class__.__name__
+        for key, values in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
 
-      return new_dict
+        return new_dict
