cfec97fca593d7d095430e29d6d80ad060ad8513
smith2eric <ericsmithsonian2@gmail.com>
2022-10-14 01:49:24 +0100

    [fix]: resolved logic and syntax errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,47 +4,63 @@ Class that serializes instances to a JSON file
 and deserializes JSON file to instances
 """
 import json
-import os
+from datetime import datetime
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
-    """ Class that serializes and deserializes JSON objects """
-    __file_path = "file.json"
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        """ Returns the dictionary __objects """
         return FileStorage.__objects
 
     def new(self, obj):
-        """ Sets in __objects the obj with key <obj class name >.id """
-        key = obj.__class__.__name__ + "." + obj.id
+        key = type(obj).__name__ + '.' + obj.id
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """ Serializes __objects to the JSON file """
-        dictionary = {}
-
-        for key, value in FileStorage.__objects.items():
-            dictionary[key] = value.to_dict()
-
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(dictionary, f)
+        """
+        serializes FileStroage.__objects
+        """
+        with open(FileStorage.__file_path, 'w+') as f:
+            dictofobjs = {}
+            for key, value in FileStorage.__objects.items():
+                dictofobjs[key] = value.to_dict()
+            json.dump(dictofobjs, f)
 
     def reload(self):
-        """ Deserializes __objects from the JSON file """
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.city import City
-        from models.amenity import Amenity
-        from models.state import State
-        from models.review import Review
-        dct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
-               'City': City, 'Amenity': Amenity, 'State': State,
-               'Review': Review}
-
-        if os.path.exists(FileStorage.__file_path) is True:
+        """
+        deserializes instances got from json file
+        """
+        try:
             with open(FileStorage.__file_path, 'r') as f:
-                for key, value in json.load(f).items():
-                    self.new(dct[value['__class__']](**value))
+                dictofobjs = json.loads(f.read())
+                from models.base_model import BaseModel
+                from models.user import User
+                for key, value in dictofobjs.items():
+                    if value['__class__'] == 'BaseModel':
+                        FileStorage.__objects[key] = BaseModel(**value)
+                    elif value['__class__'] == 'User':
+                        FileStorage.__objects[key] = User(**value)
+                    elif value['__class__'] == 'Place':
+                        FileStorage.__objects[key] = Place(**value)
+                    elif value['__class__'] == 'State':
+                        FileStorage.__objects[key] = State(**value)
+                    elif value['__class__'] == 'City':
+                        FileStorage.__objects[key] = City(**value)
+                    elif value['__class__'] == 'Amenity':
+                        FileStorage.__objects[key] = Amenity(**value)
+                    elif value['__class__'] == 'Review':
+                        FileStorage.__objects[key] = Review(**value)
+
+        except FileNotFoundError:
+            pass
