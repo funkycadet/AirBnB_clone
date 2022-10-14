95855be827e1e1fe585184cfc817a9c32217f2a2
smith2eric <ericsmithsonian2@gmail.com>
2022-10-14 02:11:23 +0100

    [update]: added tests for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,9 +4,26 @@ Test suite for base_model
 """
 import unittest
 from models.base_model import BaseModel
+from models.user import User
 
 
 class TestBaseModel(unittest.TestCase):
+    def tes_attr(self):
+        user = User()
+        self.assertEqual(user.email, '')
+        self.assertEqual(user.password, '')
+        self.assertEqual(user.first_name, '')
+        self.assertEqual(user.last_name, '')
+
+        self.assertEqual(User.email, '')
+        self.assertEqual(User.password, '')
+        self.assertEqual(User.first_name, '')
+        self.assertEqual(User.last_name, '')
+
     def test_str(self):
-        base1 = BaseModel()
-        self.assertEqual(base1.__class__, BaseModel)
+        user = User()
+        self.assertEqual(user.__class__, User)
+
+    def test_parent(self):
+        user = User()
+        self.assertTrue(isinstance(user, BaseModel))
